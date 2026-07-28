5b24b5d9d38f9d4ab4c67790b0fc175d3fcb7985
i3to <i_3to@outlook.sa>
2026-07-28 14:10:34 +0300

    Delete js directory

--- js/config.js
+++
@@ -1,4 +0,0 @@
-window.ENGLISH350_CONFIG = Object.freeze({
-  supabaseUrl: 'https://xuojgcxemheuttyitswa.supabase.co',
-  supabasePublishableKey: 'sb_publishable_ISBM7wt2Y3Hzcb3x6iTvDw_DqGMlEEe'
-});
